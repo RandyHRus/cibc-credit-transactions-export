5d580241c7657d1771699f9208f2ff8c10c8b91e
Randy Russell <25328677+RandyHRus@users.noreply.github.com>
2024-02-19 21:18:12 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 [...document.querySelector('.transaction-list').querySelectorAll('.transaction-row')].reverse().map((row) => ([
     row.querySelector('.transactionDate > span')?.innerHTML,
     row.querySelector('.transactions > span')?.innerHTML,
-    row.querySelector('.amount > span')?.innerHTML.substring(2)
+    row.querySelector('.amount > span.negative')?.innerHTML.substring(2),
+    row.querySelector('.amount > span:not(.negative)')?.innerHTML.substring(1)
 ])).map(lines => lines.join("\t")).join("\n");
